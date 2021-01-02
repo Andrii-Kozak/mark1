2649d376cf1d7f0a3722fa08c56acc33f9abeb83
Andrii Kozak <kozakdocs@gmail.com>
2021-01-02 15:06:23 +0200

    add User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   root 'static_pages#index'
+  get '/signup', to: 'users#new'
+  get '/login', to: 'sessions#new'
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,10 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
-  describe '#index' do
-    subject { get :index }
-
-    it { is_expected.to be_successful }
+  context 'home_controller routes' do
+    it 'render home#index' do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
   end
 end
