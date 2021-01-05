410b914ccafffdaeb4e898533d4e436fc832b320
Andrii Kozak <kozakdocs@gmail.com>
2021-01-05 22:55:29 +0200

    add gems dotenv and annotate

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,32 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, presence: true, length: { maximum: 50 }
-  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :first_name,
+            :last_name, presence: true, length: { maximum: 50 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, length: { maximum: 255 }
 
   def full_name
     "#{first_name} #{last_name}"
   end
 end
+
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  email                  :string(254)      not null
+#  encrypted_password     :string           default(""), not null
+#  first_name             :string(50)       not null
+#  last_name              :string(50)       not null
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#
