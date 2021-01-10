9a608cf516eebb4f74f23965e9e87e1d3fac7d78
Andrii Kozak <kozakdocs@gmail.com>
2021-01-10 23:41:17 +0200

    add User controller def-Show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root 'static_pages#index'
   get '/about', to: 'static_pages#about'
+  resources :users
 end
