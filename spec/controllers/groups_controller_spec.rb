4909b1da047974aa4062adede2dcf822949d3ec0
Andrii Kozak <kozakdocs@gmail.com>
2021-02-17 16:12:29 +0200

    fix rspec

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -28,107 +28,106 @@ RSpec.describe GroupsController, type: :controller do
     end
   end
 
-  describe 'GET#new' do
+  describe 'login_admin' do
     login_admin
-    it 'returns success and assigns group' do
-      get :new
-      expect(response).to have_http_status(:success)
-      expect(assigns(:group)).to be_a_new(Group)
-    end
-  end
 
-  describe 'POST#create' do
-    login_admin
-    context 'with valid params' do
-      it 'creates a new group' do
-        expect do
+    describe 'GET#new' do
+      it 'returns success and assigns group' do
+        get :new
+        expect(response).to have_http_status(:success)
+        expect(assigns(:group)).to be_a_new(Group)
+      end
+    end
+
+    describe 'POST#create' do
+      context 'with valid params' do
+        it 'creates a new group' do
+          expect do
+            post :create, params: { group: valid_params }
+          end.to change(Group, :count).by(1)
+        end
+
+        it 'redirects to the created group' do
           post :create, params: { group: valid_params }
-        end.to change(Group, :count).by(1)
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(group_path(Group.last))
+        end
       end
 
-      it 'redirects to the created group' do
-        post :create, params: { group: valid_params }
-        expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(group_path(Group.last))
+      context 'with invalid params' do
+        it 'do not create a new group' do
+          expect do
+            post :create, params: { group: invalid_params }
+          end.not_to change(Group, :count)
+        end
       end
     end
 
-    context 'with invalid params' do
-      it 'do not create a new group' do
-        expect do
-          post :create, params: { group: invalid_params }
-        end.not_to change(Group, :count)
-      end
-    end
-  end
-
-  describe 'GET#edit' do
-    login_admin
-    before do
-      get :edit, params: { id: group.id }
-    end
-
-    it 'returns http success and assign group' do
-      expect(response).to have_http_status(:success)
-      expect(assigns(:group)).to eq(group)
-    end
-  end
-
-  describe 'PUT#update' do
-    login_admin
-    context 'with valid params' do
+    describe 'GET#edit' do
       before do
-        put :update, params: { id: group.id,
-                               group: valid_params.merge!(group_name: 'Example',
-                                                          description: 'some description',
-                                                          group_type: 'film') }
+        get :edit, params: { id: group.id }
       end
 
-      it 'assigns the group' do
+      it 'returns http success and assign group' do
+        expect(response).to have_http_status(:success)
         expect(assigns(:group)).to eq(group)
+      end
+    end
+
+    describe 'PUT#update' do
+      context 'with valid params' do
+        before do
+          put :update, params: { id: group.id,
+                                 group: valid_params.merge!(group_name: 'Example',
+                                                            description: 'some description',
+                                                            group_type: 'film') }
+        end
+
+        it 'assigns the group' do
+          expect(assigns(:group)).to eq(group)
+          expect(response).to have_http_status(:redirect)
+          expect(response).to redirect_to(group_path(group))
+        end
+
+        it 'updates group attributes' do
+          group.reload
+          expect(group.group_name).to eq(valid_params[:group_name])
+          expect(group.description).to eq(valid_params[:description])
+          expect(group.group_type).to eq(valid_params[:group_type])
+          expect(flash[:success]).to be_present
+        end
+      end
+
+      context 'with invalid params' do
+        it 'does not change group' do
+          expect do
+            put :update, params: { id: group.id, group: invalid_params }
+          end.not_to change { group.reload.updated_at }
+        end
+      end
+
+      context 'with valid params as simple user' do
+        login_user
+
+        before do
+          put :update, params: { id: group.id, group: valid_params }
+        end
+
+        it 'does not change group' do
+          expect { group }.not_to change(group, :reload)
+          expect(response).to render_template(:show)
+        end
+      end
+    end
+
+    describe 'DELETE#destroy' do
+      it 'destroys the group and redirects to index' do
+        expect { delete :destroy, params: { id: group.id } }
+          .to change(Group, :count).by(-1)
         expect(response).to have_http_status(:redirect)
-        expect(response).to redirect_to(group_path(group))
+        expect(response).to redirect_to(groups_path)
+        expect(flash[:warning]).to be_present
       end
-
-      it 'updates group attributes' do
-        group.reload
-        expect(group.group_name).to eq(valid_params[:group_name])
-        expect(group.description).to eq(valid_params[:description])
-        expect(group.group_type).to eq(valid_params[:group_type])
-        expect(flash[:success]).to be_present
-      end
-    end
-
-    context 'with invalid params' do
-      it 'does not change group' do
-        expect do
-          put :update, params: { id: group.id, group: invalid_params }
-        end.not_to change { group.reload.updated_at }
-      end
-    end
-
-    context 'with valid params as simple user' do
-      login_user
-
-      before do
-        put :update, params: { id: group.id, group: valid_params }
-      end
-
-      it 'does not change group' do
-        expect { group }.not_to change(group, :reload)
-        expect(response).to render_template(:show)
-      end
-    end
-  end
-
-  describe 'DELETE#destroy' do
-    login_admin
-    it 'destroys the group and redirects to index' do
-      expect { delete :destroy, params: { id: group.id } }
-        .to change(Group, :count).by(-1)
-      expect(response).to have_http_status(:redirect)
-      expect(response).to redirect_to(groups_path)
-      expect(flash[:warning]).to be_present
     end
   end
 
@@ -152,19 +151,49 @@ RSpec.describe GroupsController, type: :controller do
     end
   end
 
-  # describe 'when user' do
-  #   login_user
-  #
-  #   it 'is follow to group' do
-  #     post :follow, params: { id: group.id }
-  #     expect { post :follow, params: { id: group.id } }
-  #       .to change(group.user_groups, :count).by(1)
-  #   end
-  #
-  #   it 'is unfollow from group' do
-  #     delete :unfollow, params: { id: group.id }
-  #     expect { post :unfollow, params: { id: group.id } }
-  #       .to change(group.user_groups, :count).by(-1)
-  #   end
-  # end
+  describe 'POST#follow' do
+    let!(:user) { create(:user) }
+
+    context 'when user is logged in' do
+      before do
+        sign_in(user)
+      end
+
+      it 'follow to group' do
+        expect { post :follow, params: { id: group.id } }
+          .to change(group.user_groups, :count).by(1)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'follow to group' do
+        expect { post :follow, params: { id: group.id } }
+          .to change(group.user_groups, :count).by(0)
+      end
+    end
+  end
+
+  describe 'DELETE#unfollow' do
+    let!(:user) { create(:user) }
+
+    context 'when user is logged in' do
+      before do
+        sign_in(user)
+      end
+
+      it 'unfollow to group' do
+        group.user_groups.create(user_id: user.id)
+        expect { delete :unfollow, params: { id: group.id } }
+          .to change(group.user_groups, :count).by(-1)
+      end
+    end
+
+    context 'when user is not logged in' do
+      it 'unfollow to group' do
+        group.user_groups.create(user_id: user.id)
+        expect { delete :unfollow, params: { id: group.id } }
+          .to change(group.user_groups, :count).by(0)
+      end
+    end
+  end
 end
